a9f19333cad123a73ed510e54af8dcd4d05bb583
Sean Evan Seah <dance0511@gmail.com>
2022-12-23 22:53:13 +0800

    project ourgardenstory updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,41 +1,44 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import ogsHome from "../assets/img/ogs-home.png";
+import ogsBallot from "../assets/img/ogs-ballot.png";
+import ogsHortPark from "../assets/img/ogs-hortpark.png";
+import ogsPlant from "../assets/img/ogs-plant.png";
+import ogsCommunity from "../assets/img/ogs-community.png";
+import ogsSuccess from "../assets/img/ogs-success.png";
 
 export const Projects = () => {
     const projects = [
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg1,
+            title: "Home",
+            description: "Homepage of OurGardenStory",
+            imgUrl: ogsHome,
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg2,
+            title: "Ballots",
+            description: "Overview of balloting system",
+            imgUrl: ogsBallot,
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg3,
+            title: "Hort Park Ballot",
+            description: "Balloting of an allotment garden at Hort Park",
+            imgUrl: ogsHortPark,
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg1,
+            title: "Plants",
+            description: "Plants information and how to grow them",
+            imgUrl: ogsPlant,
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg2,
+            title: "Community",
+            description: "A private community within your garden",
+            imgUrl: ogsCommunity,
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg3,
+            title: "Successful Ballot",
+            description: "Email sent to user informing of a successful ballot",
+            imgUrl: ogsSuccess,
         },
       ];
 
@@ -45,17 +48,14 @@ export const Projects = () => {
                 <Row>
                     <Col>
                     <h2>Projects</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p>I have embarked on 2 projects, OurGardenStory and Slide++. OurGardenStory was a project done in collaboration with NParks to automate the process of balloting for allotment gardens. Slide++ was a project done in my previous internship to complement SMU's ELearn system and provide additional resources for students. It is done by extracting key words from slides uploaded and then will be scraped online to search for the additional resources.</p>
                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
                         <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                             <Nav.Item>
-                                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                <Nav.Link eventKey="first">OurGardenStory</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="thrid">Tab 3</Nav.Link>
+                                <Nav.Link eventKey="second">Slide++</Nav.Link>
                             </Nav.Item>
                         </Nav>
                         <Tab.Content>
